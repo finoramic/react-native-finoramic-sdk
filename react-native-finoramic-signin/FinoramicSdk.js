08584ce9c31656e84f464125cd9cb9aa0607af2a
BalalRaza <balal.raza@gmail.com>
2018-12-06 11:42:17 +0530

    Call signin method in FinoramicSdk.js correctly, add documentation

--- react-native-finoramic-signin/FinoramicSdk.js
+++ react-native-finoramic-signin/FinoramicSdk.js
@@ -11,8 +11,13 @@ class FinoramicSignIn {
     }
   }
 
-  signIn() {
-    return this.configPromise.then(() => FinSignIn.signIn());
+  /**
+   * Sign In Function
+   * @param {String} googleClientId The web client Id of the backend server
+   * @param {String[]} extraScopes The array of extra scopes to be added as and when required by client
+   */
+  signIn(googleClientId, extraScopes = []) {
+    return this.configPromise.then(() => FinSignIn.signIn(googleClientId, extraScopes));
   }
 
   configure(options = {}) {
