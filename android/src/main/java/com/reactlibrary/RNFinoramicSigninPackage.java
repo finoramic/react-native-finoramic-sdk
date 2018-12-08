5431f056d78aabdb9271c3fcb40b66f45239d67e
BalalRaza <balal.raza@gmail.com>
2018-12-08 18:53:59 +0530

    Remove unused imports

--- android/src/main/java/com/reactlibrary/RNFinoramicSigninPackage.java
+++ android/src/main/java/com/reactlibrary/RNFinoramicSigninPackage.java
@@ -5,7 +5,6 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
